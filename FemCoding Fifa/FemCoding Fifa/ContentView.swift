7b2f6256155e896f25eaf4b7d7b576ed25add683
PolloTacticoBlindado <0277068@up.edu.mx>
2025-11-03 14:35:24 -0600

    Proyecto Creado

---
+++ FemCoding Fifa/FemCoding Fifa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FemCoding Fifa
+//
+//  Created by iOS Lab UPMX on 03/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FemCoding Fifa/FemCoding Fifa/FemCoding_FifaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FemCoding_FifaApp.swift
+//  FemCoding Fifa
+//
+//  Created by iOS Lab UPMX on 03/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FemCoding_FifaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
